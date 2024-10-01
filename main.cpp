4d7cd500993e9fb148fdd42e178be8a167ad47e7
kodai <kodai@MSI>
2024-10-01 17:01:52 +0900

    02_03完成

--- main.cpp
+++ main.cpp
@@ -1,10 +1,18 @@
 #include <cstdio>
 #include <cstdlib>
 
-int main()
+int main(int argc,char* argv[])
 {
 
-	printf("Hello,World!\n");
+	for (int i = 0; i < argc; i++)
+	{
+		//•¶Žš—ñargv‚Ìi”Ô‚ð•\Ž¦
+		printf(argv[i]);
+
+		//‰üs
+		printf("\n");
+
+	}
 
 	system("pause");
 
